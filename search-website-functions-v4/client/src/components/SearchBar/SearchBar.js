bfe49dd713a9525e7fc654baa0a0c6141973919e
Kenneth Cheung <kenche0820@gmail.com>
2024-01-06 18:34:51 -0500

    SearchBar.js revert

--- search-website-functions-v4/client/src/components/SearchBar/SearchBar.js
+++ search-website-functions-v4/client/src/components/SearchBar/SearchBar.js
@@ -1,25 +1,25 @@
 import React, {useState} from 'react';
-//import React, {useEffect} from 'react';
-//import axios from 'axios';
-//import Suggestions from './Suggestions/Suggestions';
+import React, {useEffect} from 'react';
+import axios from 'axios';
+import Suggestions from './Suggestions/Suggestions';
 
 import "./SearchBar.css";
 
 export default function SearchBar(props) {
 
     let [q] = useState("");
-/*    
+    
     let [setQ] = useState("");
 
     let [suggestions, setSuggestions] = useState([]);
     let [showSuggestions, setShowSuggestions] = useState(false);
-*/    
+    
 
     const onSearchHandler = () => {
         props.postSearchHandler(q);
-//        setShowSuggestions(false);
+        setShowSuggestions(false);
     }
-/*
+
     const suggestionClickHandler = (s) => {
         document.getElementById("search-box").value = s;
         setShowSuggestions(false);
@@ -76,20 +76,24 @@ export default function SearchBar(props) {
     } else {
         suggestionDiv = (<div></div>);
     }
-*/
+
     return (
         <div >
-            <div className="input-group">
-
+           <div className="input-group" onKeyDown={e => onEnterButton(e)}>
+                <div className="suggestions" >
                     <input 
                         autoComplete="off" // setting for browsers; not the app
                         type="text" 
                         id="search-box" 
                         className="form-control rounded-0" 
-                        placeholder="What are you looking for?"                          
-                        defaultValue={props.q}>
-                    </input>                    
-                
+                        placeholder="What are you looking for?" 
+                        onChange={onChangeHandler} 
+                        defaultValue={props.q}
+                        onBlur={() => setShowSuggestions(false)}
+                        onClick={() => setShowSuggestions(true)}>
+                    </input>
+                    {suggestionDiv}
+                </div>
                 <div className="input-group-btn">
                     <button className="btn btn-primary rounded-0" type="submit" onClick={onSearchHandler}>
                         Search
